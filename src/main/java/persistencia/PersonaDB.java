308948ed7b3c86b4f3e2307b5525fed76837a621
MelanyDayRSAI <117334084+MelanyDayRSAI@users.noreply.github.com>
2024-10-09 22:48:48 -0600

    Ingreso de Datos
    Datos de empleados

--- src/main/java/persistencia/PersonaDB.java
+++ src/main/java/persistencia/PersonaDB.java
@@ -94,8 +94,132 @@ public class PersonaDB {
            new BigDecimal("4000.75"),  // salarioBase
            new BigDecimal("580.25")    // bonificacion
         );
+        
+        // Datos de Persona 7
+        Persona persona7 = new Persona(
+            "6677889900112",            // dpi
+            "Roberto",                  // nombre1
+            "Ignacio",                  // nombre2
+            "Castillo",                 // apellido1
+            "Monterroso",               // apellido2
+            "Callejón San Juan, Zona 3",// direccionDomicilio
+            21123344,                   // telefonoDomicilio
+            56554422,                   // telefonoMovil
+            new BigDecimal("3200.30"),  // salarioBase
+            new BigDecimal("450.00")    // bonificacion
+        );
+         
+        // Datos de Persona 8
+        Persona persona8 = new Persona(
+           "1234567809876",            // dpi
+           "Paula",                    // nombre1
+           "Isabel",                   // nombre2
+           "González",                 // apellido1
+           "Cruz",                     // apellido2
+           "Residenciales del Norte, Zona 10", // direccionDomicilio
+           22660044,                   // telefonoDomicilio
+           55992211,                   // telefonoMovil
+           new BigDecimal("4400.60"),  // salarioBase
+           new BigDecimal("640.80")    // bonificacion
+        );
 
-          
+        // Datos de Persona 9
+        Persona persona9 = new Persona(
+          "0987654321098",            // dpi
+          "Ana",                      // nombre1
+          "Sofía",                    // nombre2
+          "Siana",                    // apellido1
+          "Zanches",                  // apellido2
+          "Calle de los Álamos, Zona 6",// direccionDomicilio
+           21110077,                   // telefonoDomicilio
+           55880044,                   // telefonoMovil
+           new BigDecimal("3900.20"),  // salarioBase
+           new BigDecimal("500.50")    // bonificacion
+        );
+
+        // Datos de Persona 10
+        Persona persona10 = new Persona(
+          "8765432109876",            // dpi
+          "Javier",                   // nombre1
+          "Eduardo",                  // nombre2
+          "Salazar",                  // apellido1
+          "Quiñones",                 // apellido2
+          "Avenida del Sol, Zona 9",  // direccionDomicilio
+          22002211,                   // telefonoDomicilio
+          55332288,                   // telefonoMovil
+          new BigDecimal("4300.45"),  // salarioBase
+          new BigDecimal("610.75")    // bonificacion
+        );
+
+        // Datos de Persona 11
+        Persona persona11 = new Persona(
+           "2233445566778",            // dpi
+           "Victoria",                 // nombre1
+           "Angélica",                 // nombre2
+           "Hernández",                // apellido1
+           "Castro",                   // apellido2
+           "Boulevard de las Rosas, Zona 2", // direccionDomicilio
+           22223344,                   // telefonoDomicilio
+           55668844,                   // telefonoMovil
+           new BigDecimal("3800.35"),  // salarioBase
+           new BigDecimal("450.60")    // bonificacion
+        );
+
+        // Datos de Persona 12
+        Persona persona12 = new Persona(
+           "6655443322119",            // dpi
+           "Jose",                     // nombre1
+           "Francisco",                // nombre2
+           "Vásquez",                  // apellido1
+           "Sinai",                    // apellido2
+           "Calle del Río, Zona 13",   // direccionDomicilio
+           23335566,                   // telefonoDomicilio
+           55221100,                   // telefonoMovil
+           new BigDecimal("4600.25"),  // salarioBase
+           new BigDecimal("670.00")    // bonificacion
+        );
+
+        // Datos de Persona 13
+        Persona persona13 = new Persona(
+            "7766554433221",            // dpi
+            "Raquel",                   // nombre1
+            "Marina",                   // nombre2
+            "Escobar",                  // apellido1
+            "Domínguez",                // apellido2
+            "Avenida 15, Zona 14",      // direccionDomicilio
+            21110066,                   // telefonoDomicilio
+            55667700,                   // telefonoMovil
+            new BigDecimal("4100.10"),  // salarioBase
+            new BigDecimal("520.50")    // bonificacion
+        );
+
+       // Datos de Persona 14
+       Persona persona14 = new Persona(
+          "4455667788990",            // dpi
+          "David",                    // nombre1
+          "Alonso",                   // nombre2
+          "Medina",                   // apellido1
+          "López",                    // apellido2
+          "Calle 9, Zona 4",          // direccionDomicilio
+          22007711,                   // telefonoDomicilio
+          55443300,                   // telefonoMovil
+          new BigDecimal("3900.80"),  // salarioBase
+          new BigDecimal("500.25")    // bonificacion
+        );
+
+       // Datos de Persona 15
+       Persona persona15 = new Persona(
+          "1122334455667",            // dpi
+          "Mónica",                   // nombre1
+          "Claudia",                  // nombre2
+          "Campos",                   // apellido1
+          "Reyes",                    // apellido2
+          "Colonia Los Pinos, Zona 5",// direccionDomicilio
+          22110022,                   // telefonoDomicilio
+          55990077,                   // telefonoMovil
+          new BigDecimal("4500.35"),  // salarioBase
+          new BigDecimal("680.50")    // bonificacion
+        );
 
         // Agregar personas a la lista
         listaPersonas.add(persona1);
@@ -104,7 +228,15 @@ public class PersonaDB {
         listaPersonas.add(persona4);
         listaPersonas.add(persona5);
         listaPersonas.add(persona6);
-
+        listaPersonas.add(persona7);
+        listaPersonas.add(persona8);
+        listaPersonas.add(persona9);
+        listaPersonas.add(persona10);
+        listaPersonas.add(persona11);
+        listaPersonas.add(persona12);
+        listaPersonas.add(persona13);
+        listaPersonas.add(persona14);
+        listaPersonas.add(persona15);
         
         // Retornar la lista de personas
         return listaPersonas;
